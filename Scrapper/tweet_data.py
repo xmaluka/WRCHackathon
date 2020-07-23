b407c0062a231fe4a30502b1abefc8a0118a1ebd
xmaluka <marciomaluka@ymail.com>
2020-07-23 17:42:22 +0200

    new apis,credentials

--- Scrapper/tweet_data.py
+++ Scrapper/tweet_data.py
@@ -4,10 +4,12 @@ from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("link-tweets-88d96c00cd13.json", scope)
 
 client = gspread.authorize(creds)
 
-sheet = client.open("tutorial").sheet1  # Open the spreadhseet
+sheet = client.open("tweet_to_leak").sheet1  # Open the spreadhseet
 
 data = sheet.get_all_records()  # Get a list of all record
+
+print(data)
